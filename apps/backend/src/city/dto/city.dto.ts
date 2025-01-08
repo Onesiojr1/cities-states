4d3fc0e9fd0bb3778eb7182e94c28b9085ae1f0e
Onesio Junior <onesiojr1@gmail.com>
2025-01-08 16:05:38 -0300

    feat: add acronym to states

--- apps/backend/src/city/dto/city.dto.ts
+++ apps/backend/src/city/dto/city.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from "class-validator";
+
 export class cityDto {
+  @IsString()
   name: string;
+  
+  @IsString()
   stateId: string;
 }
--- apps/backend/src/state/dto/state.dto.ts
+++ apps/backend/src/state/dto/state.dto.ts
@@ -1,3 +1,10 @@
+import { IsString, Length } from "class-validator";
+
 export class StateDto {
+  @IsString()
   name: string;
+
+  @IsString()
+  @Length(2, 2)
+  acronym: string;
 }
