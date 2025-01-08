3e78b87acb64af0c43d7fc393202981287f7a2be
Onesio Junior <onesiojr1@gmail.com>
2025-01-08 19:43:34 -0300

    feat: create header

---
+++ apps/frontend/src/app/components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Container from "./Container";
+
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 w-full border-b border-transparent-white backdrop-blur-[12px]">
+      <Container className="flex justify-center h-20 gap-5">
+        <Link className="flex items-center text-xl" href="/">
+          <span className="text-white font-extrabold">Cidades</ span>
+        </Link>
+        <Link className="flex items-center text-xl" href="/state">
+          <span className="text-white font-extrabold">Estados</ span>
+        </Link>
+      </Container>
+    </header>
+  );
+}
