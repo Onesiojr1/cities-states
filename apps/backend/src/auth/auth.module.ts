4fafa74dcf8d9a5c5230f03daa41a688f34cf092
Onesio Junior <onesiojr1@gmail.com>
2025-01-10 17:08:42 -0300

    fix: remove unused import

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
